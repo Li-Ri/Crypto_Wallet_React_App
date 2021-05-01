936e571ddaba65c5c2e488b27a4e201f0113478f
Liam <liam.richens.11@gmail.com>
2021-05-01 14:10:02 +0100

    fetch apis

--- server/helpers/fetch_APIs.js
+++ server/helpers/fetch_APIs.js
@@ -1,16 +1,25 @@
 require("dotenv").config({ path: "../.env" });
 const axios = require("axios");
 
-const finage_key = `https://api.finage.co.uk/last/crypto/detailed/btcusd?apikey=${process.env.API_KEY_FINAGE}`;
-const alpha = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=BTC&interval=5min&api=${process.env.API_KEY_ALPHAVANTAGE}`;
-
+const finage = `https://api.finage.co.uk/last/crypto/detailed/btcusd?apikey=${process.env.API_KEY_FINAGE}`;
+const alpha = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=BTC&interval=5min&apikey=${process.env.API_KEY_ALPHAVANTAGE}`;
+console.log(finage, alpha);
 const getApi = (url) => {
   return axios.get(url);
 };
 
-Promise.all([getApi(finage_key), getApi(alpha)]).then((results) => {
-  const finage = results[0];
-  const alpha = results[1];
+const new_object = {
+  name: "",
+  symbol: "",
+  currentPrice: 0,
+};
+
+Promise.all([getApi(finage), getApi(alpha)]).then((results) => {
+  new_object.name = results[0].name;
+  new_object.symbol = results[0].symbol;
+  new_object.currentPrice = results[0].price;
 });
 
-export default getCryptos;
+setTimeout(() => {
+  console.log(new_object);
+}, 4000);
