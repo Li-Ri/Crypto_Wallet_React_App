42f4c21b25b5c58f110f433f9ba924c6816aaf35
Liam <liam.richens.11@gmail.com>
2021-05-01 15:10:09 +0100

    api successful

--- server/helpers/fetch_APIs.js
+++ server/helpers/fetch_APIs.js
@@ -1,9 +1,9 @@
 require("dotenv").config({ path: "../.env" });
 const axios = require("axios");
-
+const MongoClient = require("mongo").MongoClient;
 const finage = `https://api.finage.co.uk/last/crypto/detailed/btcusd?apikey=${process.env.API_KEY_FINAGE}`;
 const alpha = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=BTC&interval=5min&apikey=${process.env.API_KEY_ALPHAVANTAGE}`;
-console.log(finage, alpha);
+
 const getApi = (url) => {
   return axios.get(url);
 };
@@ -12,12 +12,16 @@ const new_object = {
   name: "",
   symbol: "",
   currentPrice: 0,
+  history: [],
+  priceChange: 0,
 };
 
 Promise.all([getApi(finage), getApi(alpha)]).then((results) => {
-  new_object.name = results[0].name;
-  new_object.symbol = results[0].symbol;
-  new_object.currentPrice = results[0].price;
+  new_object.name = results[0].data.name;
+  new_object.symbol = results[0].data.symbol;
+  new_object.currentPrice = results[0].data.price;
+  new_object.priceChange = results[0].data.changesPercentage;
+  new_object.history = Object.entries(results[1].data["Time Series (5min)"]);
 });
 
 setTimeout(() => {
--- server/server.js
+++ server/server.js
@@ -1,23 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const cors = require('cors');
-app.use.(cors());
+const cors = require("cors");
+app.use(cors());
 app.use(express.json());
 
-const MongoClient = require('mongodb').MongoClient;
-const createRouter = require('./helpers/create_route.js');
+const MongoClient = require("mongodb").MongoClient;
+const createRouter = require("./helpers/create_route.js");
 
-MongoClient.connect('mongodb://localhost:27017')
-    .then((client) => {
-        const db = client.db('crypto_db')
-    ;
-    const cryptosCollection = db.collection('cryptos');
-    const cryptosRouter = createRouter(cryptosCollection)
-    app.use('api/cryptos', cryptosRouter)
-    })
-    .catch(console.err);
+MongoClient.connect("mongodb://localhost:27017")
+  .then((client) => {
+    const db = client.db("crypto_db");
+    const cryptosCollection = db.collection("cryptos");
+    // const cryptosRouter = createRouter(cryptosCollection)
+    // app.use('api/cryptos', cryptosRouter)
+  })
+  .catch((err) => console.error(err));
 
-    app.listen(5000, function(){
-        console.log(`Listening on port ${this.address().port}`)
-    })
+app.listen(5000, function () {
+  console.log(`Listening on port ${this.address().port}`);
+});
