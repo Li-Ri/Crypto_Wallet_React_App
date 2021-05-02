91894ccd38556b1c29cf4c6327589d78bdf29298
Liam <liam.richens.11@gmail.com>
2021-05-02 16:08:54 +0100

    minor fix

--- client/src/components/DashBoard.js
+++ client/src/components/DashBoard.js
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from "react";
 import { getCryptos } from "../services/ApiServices";
 
 const DashBoard = () => {
-  const [stocks, setStocks] = useState();
-  const [user, setUser] = useState();
+  const [stocks, setStocks] = useState([]);
+  const [user, setUser] = useState({});
 
   const fetchStock = () => {
     getCryptos().then((cryptos) => setStocks(cryptos));
