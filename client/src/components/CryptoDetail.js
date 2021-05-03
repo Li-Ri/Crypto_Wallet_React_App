098242887c302ee60025843abb803b6953b086f6
Liam <liam.richens.11@gmail.com>
2021-05-03 14:05:08 +0100

    crypto detail bug fixed

--- client/src/components/CryptoDetail.js
+++ client/src/components/CryptoDetail.js
@@ -2,9 +2,12 @@ import React from "react";
 
 const CryptoDetail = ({ user, stock }) => {
   let userStock;
-
+  if (user.stock_units !== undefined) {
+    userStock = user.stock_units[stock.symbol];
+  }
   return (
     <>
+      <h2>Stock Owned:{userStock}</h2>
       <h1>{stock.name}</h1>
       <h3>{stock.symbol}</h3>
       <h3>Current Price: {stock.currentPrice}</h3>
--- client/src/components/Portfolio.js
+++ client/src/components/Portfolio.js
@@ -5,7 +5,7 @@ const Portfolio = ({ user, stocks }) => {
   let userStockItems;
   if (user.portfolio) {
     userStockItems = user.portfolio.map((stock) => {
-      return <CryptoDetail stock={stock} />;
+      return <CryptoDetail user={user} stock={stock} />;
     });
   }
   return <>{userStockItems}</>;
