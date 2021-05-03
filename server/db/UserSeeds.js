904af2ea133d0ab9fadbfeda7acd7773699eb9ac
Liam <liam.richens.11@gmail.com>
2021-05-03 16:04:38 +0100

    add remove cash added

--- server/db/UserSeeds.js
+++ server/db/UserSeeds.js
@@ -1,5 +1,5 @@
 
 use crypto_db;
 
-db.users.insertOne({name:"Michael Jackson", email:"neverlandranch@email.com", cash:100000, portfolio:[],stock_units:{}, invested:0});
+db.users.insertOne({name:"Michael Jackson", email:"neverlandranch@email.com", cash:100000, portfolio:[{"_id":"608d6bfb1b1182a5d0685b70","name":"Bitcoin Gold USD","symbol":"BTGUSD","currentPrice":95.87935,"history":[],"priceChange":0.3}],stock_units:{}, invested:0});
 
