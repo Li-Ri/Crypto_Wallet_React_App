0a0d0f0e98d58f3c36baf880efdab8a66e6bd6cd
Liam <liam.richens.11@gmail.com>
2021-05-03 14:01:09 +0100

    detail added

--- client/src/components/CryptoDetail.js
+++ client/src/components/CryptoDetail.js
@@ -1,10 +1,14 @@
 import React from "react";
 
-const CryptoDetail = ({ users, stock }) => {
+const CryptoDetail = ({ user, stock }) => {
+  let userStock;
+
   return (
     <>
       <h1>{stock.name}</h1>
-      <h3>{stock.currentPrice}</h3>
+      <h3>{stock.symbol}</h3>
+      <h3>Current Price: {stock.currentPrice}</h3>
+      <h3>Price Change: {stock.priceChange}</h3>
     </>
   );
 };
--- client/src/components/Portfolio.js
+++ client/src/components/Portfolio.js
@@ -2,11 +2,13 @@ import React from "react";
 import CryptoDetail from "./CryptoDetail";
 
 const Portfolio = ({ user, stocks }) => {
-  const stockItems = stocks.map((stock) => {
-    return <CryptoDetail user={user} stock={stock} />;
-  });
-
-  return <div>{stockItems}</div>;
+  let userStockItems;
+  if (user.portfolio) {
+    userStockItems = user.portfolio.map((stock) => {
+      return <CryptoDetail stock={stock} />;
+    });
+  }
+  return <>{userStockItems}</>;
 };
 
 export default Portfolio;
