f9647df049ad049064acc2c3d3dc725e426bad2c
Liam <liam.richens.11@gmail.com>
2021-05-03 15:33:22 +0100

    Stock Value Bug Fixed

--- client/src/components/StockValue.js
+++ client/src/components/StockValue.js
@@ -1,17 +1,16 @@
 import React from "react";
 
 const StockValue = ({ user, stocks }) => {
-  //   let stocksInvested;
-  //   let totalValue = stocks.reduce((accum, current) => {
-  //     if (user.stock_units[current.symbol] !== undefined) {
-  //       accum += current.currentPrice * user.stock_units[current.symbol];
-  //     }
-  //     return accum;
-  //   }, 0);
+  let stocksInvested;
+  let totalValue = stocks.reduce((accum, current) => {
+    if (user.stock_units && user.stock_units[current.symbol] !== undefined) {
+      accum += current.currentPrice * user.stock_units[current.symbol];
+    }
+    return accum;
+  }, 0);
 
-  //   console.log(totalValue);
-  //   return <>{totalValue == NaN || totalValue === 0 ? null : totalValue}</>;
-  return <h1>hello</h1>;
+  console.log(totalValue);
+  return <>{totalValue == NaN || totalValue === 0 ? null : totalValue}</>;
 };
 
 export default StockValue;
