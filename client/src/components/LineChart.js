de5ceef12b40c957987d45c2b84a751874959d05
Liam <liam.richens.11@gmail.com>
2021-05-04 09:08:44 +0100

    add conditional background colors for stock prices

--- client/src/components/LineChart.js
+++ client/src/components/LineChart.js
@@ -4,15 +4,20 @@ import FakeStock from "fake-stock-market-generator";
 
 const LineChart = () => {
   let stockData = FakeStock.generateStockData(20);
-  console.log(stockData);
+  const stockArray = stockData.priceData.map((data) => data.price);
+  const backgroundColor =
+    stockArray[19] > stockArray[18]
+      ? "rgba(35, 203, 167, 1)"
+      : "rgba(210, 77, 87, 0.8)";
+  console.log(backgroundColor);
   const data = {
     labels: stockData.priceData.map((data) => data.minute),
     datasets: [
       {
         label: "# of Votes",
         data: stockData.priceData.map((data) => data.price),
-        fill: false,
-        backgroundColor: "rgb(255, 99, 132)",
+        fill: true,
+        backgroundColor: backgroundColor,
         borderColor: "rgba(255, 99, 132, 0.2)",
       },
     ],
