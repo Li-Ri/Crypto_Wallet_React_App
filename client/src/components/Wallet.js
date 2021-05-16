4c7bf42962ed243e50266ad1818311c04bde5279
Liam <liam.richens.11@gmail.com>
2021-05-16 21:30:30 +0100

    UI tests added for dashboard

--- client/src/components/Wallet.js
+++ client/src/components/Wallet.js
@@ -9,18 +9,22 @@ const Wallet = ({ user, addRemoveCash }) => {
     <>
       <div className="stat">
         <h2>Wallet</h2>
-        <h3 data-testid="wallet-total">
+        <h3 data-testid="wallet-total" id="wallet-total">
           ${user.cash ? user.cash.toFixed(2).toString() : null}
         </h3>
         <h4>
-          <button data-testid="add-money" onClick={handleClick}>
+          <button data-testid="add-money" id="add-money" onClick={handleClick}>
             Add Cash to Account
           </button>
         </h4>
         <form onSubmit={addRemoveCash} className="hidden" id="add">
           <label htmlFor="amount">Amount</label>
           <input data-testid="wallet-form" type="text" id="amount" />
-          <input data-testid="wallet-form-submit" type="submit" />
+          <input
+            data-testid="wallet-form-submit"
+            id="wallet-form-submit"
+            type="submit"
+          />
         </form>
       </div>
     </>
