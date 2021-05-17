8c61b8faa27313ce81ceec7d34c2d49041280462
Liam <liam.richens.11@gmail.com>
2021-05-17 21:33:20 +0100

    complete login functionality

--- server/db/UserSeeds.js
+++ server/db/UserSeeds.js
@@ -1,5 +1,5 @@
 
 use crypto_db;
 
-db.users.insertOne({name:"Michael Jackson", email:"neverlandranch@email.com", cash:100000, portfolio:[],stock_units:{}, invested:0});
+db.users.insertOne({name:"Michael",password:'test123', email:"neverlandranch@email.com", cash:100000, portfolio:[],stock_units:{}, invested:0});
 
